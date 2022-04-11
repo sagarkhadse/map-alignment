8958ccee23acf7dacffbfdd5013fddc05155e752
sagarkhadse <sagarkhadse@gmail.com>
2022-04-11 14:04:00 -0400

    testing getting roi from mask

--- map_align.py
+++ map_align.py
@@ -24,19 +24,21 @@ class map_align:
     def preproc(self, img):
         _, img = cv.threshold(img, 200, 255, cv.THRESH_BINARY)
         img = cv.morphologyEx(img, cv.MORPH_DILATE, self._k5)
-        # img = cv.morphologyEx(img, cv.MORPH_GRADIENT, self._k5)
-
+        cv.imwrite("img_preproc.jpg", img)
         return img
 
-        img_lines = cv.cvtColor(np.zeros(img.shape, np.uint8), cv.COLOR_GRAY2BGR)
-            
+    def hough(self, img):
+        img = cv.morphologyEx(img, cv.MORPH_CLOSE, self._k3)
+        # img = cv.morphologyEx(img, cv.MORPH_GRADIENT, self._k5)
+        img_lines = cv.copyTo(img, None)
+        # img_lines = cv.cvtColor(np.zeros(img.shape, np.uint8), cv.COLO)
         linesP = cv.HoughLinesP(img, 1, np.pi / 180, 50, None, 50, 10)
-
         if linesP is not None:
             for i in range(0, len(linesP)):
                 l = linesP[i][0]
-                cv.line(img_lines, (l[0], l[1]), (l[2], l[3]), (255,255,255), 1, cv.LINE_AA)
-        
+                cv.line(img_lines, (l[0], l[1]), (l[2], l[3]), (255,255,255), 3, cv.LINE_AA)
+        img = cv.morphologyEx(img_lines, cv.MORPH_DILATE, self._k3)
+        cv.imwrite("img_hough.jpg", img_lines)
         return img_lines
 
     def approximate(self, mask):
@@ -45,37 +47,120 @@ class map_align:
         for cnt in contours:
             area = cv.contourArea(cnt)
             if area > 500:
-                epsilon = 0.001*cv.arcLength(cnt, True)
+                epsilon = 0.005*cv.arcLength(cnt, True)
                 approx = cv.approxPolyDP(cnt, epsilon, True)
                 cv.drawContours(img, [approx], -1, (255, 255, 255), 3)
-        cv.imwrite("approx.jpg", img)
+        cv.imwrite("img_approx.jpg", img)
         return img
 
     def sift_match(self, map, query):
         # Initiate SIFT detector
-        sift = cv.xfeatures2d.SIFT_create()
+        sift = cv.SIFT_create()
+
         # find the keypoints and descriptors with SIFT
         kp1, des1 = sift.detectAndCompute(map,None)
         kp2, des2 = sift.detectAndCompute(query,None)
+
         # FLANN parameters
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
         search_params = dict(checks=50)   # or pass empty dictionary
         flann = cv.FlannBasedMatcher(index_params,search_params)
         matches = flann.knnMatch(des1,des2,k=2)
+
+        ratio_thresh = 0.75
+        good_matches = []
+        for m,n in matches:
+            if m.distance < ratio_thresh * n.distance:
+                good_matches.append(m)
+
         # Need to draw only good matches, so create a mask
         matchesMask = [[0,0] for i in range(len(matches))]
         # ratio test as per Lowe's paper
         for i,(m,n) in enumerate(matches):
-            if m.distance < 0.7*n.distance:
+            if m.distance < ratio_thresh*n.distance:
                 matchesMask[i]=[1,0]
+        
         draw_params = dict(matchColor = (0,255,0),
                         singlePointColor = (255,0,0),
                         matchesMask = matchesMask,
                         flags = cv.DrawMatchesFlags_DEFAULT)
+        
         img = cv.drawMatchesKnn(map,kp1,query,kp2,matches,None,**draw_params)
+        cv.imwrite("sift.jpg", img)
 
-        return img
+        # Extract location of good matches
+        points1 = np.zeros((len(matches), 2), dtype=np.float32)
+        points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+        for i, match in enumerate(good_matches):
+            points1[i, :] = kp1[match.queryIdx].pt
+            points2[i, :] = kp2[match.trainIdx].pt
+
+        # Find homography
+        h, mask = cv.findHomography(points1, points2, cv.RANSAC)
+
+        # Use homography
+        height, width = query.shape
+        mapReg = cv.warpPerspective(map, h, (width, height))
+        cv.imwrite("sift_reg.jpg", mapReg)
+        return mapReg
+
+    def surf_match(self, map, query):
+        img_object = map
+        img_scene = query
+
+        #-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+        minHessian = 400
+        detector = cv.SIFT_create()
+        keypoints_obj, descriptors_obj = detector.detectAndCompute(img_object, None)
+        keypoints_scene, descriptors_scene = detector.detectAndCompute(img_scene, None)
+        #-- Step 2: Matching descriptor vectors with a FLANN based matcher
+        # Since SURF is a floating-point descriptor NORM_L2 is used
+        matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
+        knn_matches = matcher.knnMatch(descriptors_obj, descriptors_scene, 2)
+        #-- Filter matches using the Lowe's ratio test
+        ratio_thresh = 0.75
+        good_matches = []
+        for m,n in knn_matches:
+            if m.distance < ratio_thresh * n.distance:
+                good_matches.append(m)
+        #-- Draw matches
+        img_matches = np.empty((max(img_object.shape[0], img_scene.shape[0]), img_object.shape[1]+img_scene.shape[1], 3), dtype=np.uint8)
+        cv.drawMatches(img_object, keypoints_obj, img_scene, keypoints_scene, good_matches, img_matches, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        #-- Localize the object
+        obj = np.empty((len(good_matches),2), dtype=np.float32)
+        scene = np.empty((len(good_matches),2), dtype=np.float32)
+        for i in range(len(good_matches)):
+            #-- Get the keypoints from the good matches
+            obj[i,0] = keypoints_obj[good_matches[i].queryIdx].pt[0]
+            obj[i,1] = keypoints_obj[good_matches[i].queryIdx].pt[1]
+            scene[i,0] = keypoints_scene[good_matches[i].trainIdx].pt[0]
+            scene[i,1] = keypoints_scene[good_matches[i].trainIdx].pt[1]
+        H, _ =  cv.findHomography(obj, scene, cv.RANSAC)
+        #-- Get the corners from the image_1 ( the object to be "detected" )
+        obj_corners = np.empty((4,1,2), dtype=np.float32)
+        obj_corners[0,0,0] = 0
+        obj_corners[0,0,1] = 0
+        obj_corners[1,0,0] = img_object.shape[1]
+        obj_corners[1,0,1] = 0
+        obj_corners[2,0,0] = img_object.shape[1]
+        obj_corners[2,0,1] = img_object.shape[0]
+        obj_corners[3,0,0] = 0
+        obj_corners[3,0,1] = img_object.shape[0]
+        scene_corners = cv.perspectiveTransform(obj_corners, H)
+        #-- Draw lines between the corners (the mapped object in the scene - image_2 )
+        cv.line(img_matches, (int(scene_corners[0,0,0] + img_object.shape[1]), int(scene_corners[0,0,1])),\
+            (int(scene_corners[1,0,0] + img_object.shape[1]), int(scene_corners[1,0,1])), (0,255,0), 4)
+        cv.line(img_matches, (int(scene_corners[1,0,0] + img_object.shape[1]), int(scene_corners[1,0,1])),\
+            (int(scene_corners[2,0,0] + img_object.shape[1]), int(scene_corners[2,0,1])), (0,255,0), 4)
+        cv.line(img_matches, (int(scene_corners[2,0,0] + img_object.shape[1]), int(scene_corners[2,0,1])),\
+            (int(scene_corners[3,0,0] + img_object.shape[1]), int(scene_corners[3,0,1])), (0,255,0), 4)
+        cv.line(img_matches, (int(scene_corners[3,0,0] + img_object.shape[1]), int(scene_corners[3,0,1])),\
+            (int(scene_corners[0,0,0] + img_object.shape[1]), int(scene_corners[0,0,1])), (0,255,0), 4)
+        #-- Show detected matches
+        cv.imwrite('surf.jpg', img_matches)
+
 
     def registration(self, map, query):
 
@@ -97,7 +182,7 @@ class map_align:
 
         # Draw top matches
         imMatches = cv.drawMatches(map, keypoints1, query, keypoints2, matches, None)
-        cv.imwrite("matches.jpg", imMatches)
+        cv.imwrite("orb.jpg", imMatches)
 
         # Extract location of good matches
         points1 = np.zeros((len(matches), 2), dtype=np.float32)
@@ -116,36 +201,40 @@ class map_align:
 
         return mapReg, h
 
-    def get_roi(self, map, query):
+    def get_roi(self, map, map_mask, query):
         w, h = query.shape[::-1]
         method = eval('cv.TM_CCOEFF_NORMED')
 
-        res = cv.matchTemplate(map,query,method)
+        res = cv.matchTemplate(map_mask,query,method)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
         roi = cv.cvtColor(map, cv.COLOR_GRAY2RGB)
         cv.rectangle(roi, top_left, bottom_right, (255, 0, 0), 2)
         cv.imwrite('roi.jpg', roi)
-        return map[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+        mask = np.zeros(map_mask.shape[:2], dtype="uint8")
+        cv.rectangle(mask, top_left, bottom_right, 255, -1)
+        masked = cv.bitwise_and(map_mask, map_mask, mask=mask)
+        cv.imwrite('roi_mask.jpg', masked)
+
+        return masked
 
 if __name__ == '__main__':
 
     img_path = './images/slam_map_full.png'
     map_mask_path = './images/map_mask_2.png'
+    map_path = './images/map.png'
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
     map_mask = cv.imread(map_mask_path, cv.IMREAD_GRAYSCALE)
+    map = cv.imread(map_path, cv.IMREAD_GRAYSCALE)
 
     align = map_align()
-    edges = align.edges(img)
-    img_lines = align.preproc(img)
-    approx = align.approximate(img_lines)
-    
-    
-    roi = align.get_roi(map_mask, img_lines)
-    sift_img = align.sift_match(roi, img_lines)
-    mapReg, h = align.registration(img_lines, roi);
-    print(h)
 
-    plt.imshow(mapReg, 'gray')
-    plt.show()
+    img = align.preproc(img)
+    img = align.hough(img)
+
+    roi = align.get_roi(map, map_mask, img)
+    sift_match = align.sift_match(img, roi)
+    surf_match = align.surf_match(img, roi)
+    mapReg, h = align.registration(roi, img)
